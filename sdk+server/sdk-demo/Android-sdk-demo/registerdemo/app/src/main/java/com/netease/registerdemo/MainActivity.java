54575f302c5bec1ae0fcea82899d2384744111bf
wangzh7678 <170069212@qq.com>
2019-12-20 17:35:13 +0800

    Update MainActivity.java

--- sdk+server/sdk-demo/Android-sdk-demo/registerdemo/app/src/main/java/com/netease/registerdemo/MainActivity.java
+++ sdk+server/sdk-demo/Android-sdk-demo/registerdemo/app/src/main/java/com/netease/registerdemo/MainActivity.java
@@ -6,15 +6,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.netease.mobsec.rjsb.watchman;
+import com.netease.mobsec.rjsb.RequestCallback;
 
 public class MainActivity extends Activity {
     private EditText mEtUsername, mEtPsd , mEtNickname;
     private Button mBtRegister;
-
+    public statc String TAG= "TestLog";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        watchman.init(getApplicationContext(), "your productNumber");
+        watchman.init(getApplicationContext(), "your productNumber",new RequestCallback(){
+            @Override
+            public void onResult(int code, String msg) {
+                Log.e(TAG,"init,code = " + code + " msg = " + msg);
+            }
+        });
         setContentView(R.layout.activity_main);
 
         mEtUsername = (EditText)findViewById(R.id.et_username);
